d14d21465f0d58a15c14e4f8a49714721718e997
fabiandariomorenor <fdmorenor@unal.edul.co>
2013-05-04 11:12:39 -0500

    Desarrolllo del agoritmo SimpleMovingAverage y de la clase controladora WeatherForecaster

--- VirtualWeatherForecaster/src/Algorithms/SimpleMovingAverage.java
+++ VirtualWeatherForecaster/src/Algorithms/SimpleMovingAverage.java
@@ -3,6 +3,10 @@
  * and open the template in the editor.
  */
 package Algorithms;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
 
 /**
  *
@@ -10,4 +14,29 @@ package Algorithms;
  */
 public class SimpleMovingAverage {
     
+    public static ArrayList<Double> Get(ArrayList<Double> last, int days ){
+        
+        ArrayList<Double> lstForecast = new ArrayList<Double>();
+        
+        for(int j=0;j<days;j++)
+        {
+            int t=last.size();
+            
+            //si la lista contiene elementos
+            if(t>0)
+            {
+                double total = 0;
+
+                for(int i=0 ; i < t; i++)
+                {
+                    total += last.get(i);
+                }
+
+                total = total/t;
+                last.add(total);
+                lstForecast.add(total);
+            }
+        }
+        return lstForecast;
+    }
 }
