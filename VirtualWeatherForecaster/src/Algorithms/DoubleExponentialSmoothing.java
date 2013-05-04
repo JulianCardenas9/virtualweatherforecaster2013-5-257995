778917a1890002258c10d4c33dab17546300232f
Nicolas Camelo <jncamelog@unal.edu.co>
2013-05-04 10:34:13 -0500

    Clases algoritmos
    Commit By: Nicolas Camelo
    
    Changes: Clases para algoritmos de forecasting

---
+++ VirtualWeatherForecaster/src/Algorithms/DoubleExponentialSmoothing.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algorithms;
+
+/**
+ *
+ * @author Juancho
+ */
+public class DoubleExponentialSmoothing {
+    
+}
