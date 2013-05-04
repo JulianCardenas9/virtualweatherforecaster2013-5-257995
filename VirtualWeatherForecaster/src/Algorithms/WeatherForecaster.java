a7913930b66130bfd34d6b981b125f382c0deb0e
fabiandariomorenor <fdmorenor@unal.edul.co>
2013-05-04 12:41:23 -0500

    Desarrolllo de los eventos de la interfaz

---
+++ VirtualWeatherForecaster/src/Algorithms/WeatherForecaster.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algorithms;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Fabian
+ */
+public class WeatherForecaster {
+    
+    public ArrayList<Double> Get(ArrayList<Double> last, int days ){
+        
+        // calcular SimpleMovingAverage
+        ArrayList<Double> lstSimpleMovingAverage = SimpleMovingAverage.Get(last, days);
+        
+        // calcular DoubleMovingAverage
+        ArrayList<Double> lstDoubleMovingAverage = DoubleMovingAverage.Get(last, days);
+        
+        
+        return lstDoubleMovingAverage;
+    }
+}
